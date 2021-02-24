9cff8a5c86426af0c3f6b30340c20d48b2c45c24
Soheil Movahedan <soheil.movahhedan@gmail.com>
2021-02-24 07:00:42 +0330

    Config tailwind for CRA using craco

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,23 +6,21 @@ module.exports = {
       md: '768px',
       lg: '1024px',
     },
-    colors: {
-      transparent: 'transparent',
-      black: '#000000',
-      white: '#FFFFFF',
-      primary: {
-        default: '#424242',
-        light: '#757575',
-        dark: '#212121',
-      },
-      secondary: {
-        default: '#1c9eb2',
-        light: '#84ffff',
-      },
-      gray: {
-        default: '#ededed',
-        light: '#f5f5f5',
-        dark: '#bcbcbc',
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        black: '#000000',
+        white: '#FFFFFF',
+        primary: '#424242',
+        'primary-light': '#757575',
+        'primary-dark': '#212121',
+        secondary: '#1c9eb2',
+        'secondary-light': '#84ffff',
+        gray: {
+          default: '#ededed',
+          light: '#f5f5f5',
+          dark: '#bcbcbc',
+        },
       },
     },
   },
